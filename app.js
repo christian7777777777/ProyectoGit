a9c9ad7f1d38957acb73b38e8a5742249ce71a77
Christian M. Oyaque II <150096015+Manuman1505@users.noreply.github.com>
2023-11-06 22:18:03 -0500

    "Version Manuman 4.0"
    "Version Manuman 4.0"

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hoila mundo');
+document.addEventListener('DOMContentLoaded', function() {
+    const registrationForm = document.getElementById('registrationForm');
+
+    registrationForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Aquí puedes agregar código para procesar el registro, como enviar los datos a un servidor o almacenarlos localmente.
+
+        // Por ahora, simplemente mostraremos un mensaje en la consola.
+        console.log(`Usuario: ${username}, Contraseña: ${password}`);
+
+        // Si deseas realizar alguna acción adicional después del registro, puedes hacerlo aquí.
+    });
+});
+
